eb9bbee206418ac3bb827881334b75dbf098b3f6
Blake Lindsay <blake@gotmonkey.com>
2020-03-27 10:33:27 -0700

    TCR remove # style private field from test

--- test/test-registrar.ts
+++ test/test-registrar.ts
@@ -2,16 +2,16 @@ import { ExamResult, Registrar } from "../src";
 
 export class TestRegistrar implements Registrar
 {
-	readonly #emptyResult: ExamResult = {} as ExamResult;
-	readonly #examResults: ExamResult[] = [];
+	private readonly emptyResult: ExamResult = {} as ExamResult;
+	private readonly examResults: ExamResult[] = [];
 
 	public async record(examResult: Readonly<ExamResult>): Promise<void>
 	{
-		this.#examResults.push(examResult);
+		this.examResults.push(examResult);
 	}
 
 	public popLastRecord(): ExamResult
 	{
-		return this.#examResults.pop() || this.#emptyResult;
+		return this.examResults.pop() || this.emptyResult;
 	}
 }
