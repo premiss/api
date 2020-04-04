6897a1f41259cbc75b118c7e2260ef3bc4205eb3
Blake Lindsay <blake@gotmonkey.com>
2020-04-03 18:51:16 -0700

    TCR time each test and ensure all are run

--- test/core-tests/core-test-runner.ts
+++ test/core-tests/core-test-runner.ts
@@ -1,15 +1,31 @@
-import { timedAsyncCall } from "../../src/timing";
+import { timedAsyncCall, TimedResult } from "../../src/timing";
 import { emptyAssertPassTest, exceptionalActFailTest, exceptionalAnnulFailTest, exceptionalArrangeFailTest, exceptionalAssertFailTest } from "./";
+
+const runTest = async (test: () => Promise<void>): Promise<TimedResult<Error | undefined>> =>
+{
+	return await timedAsyncCall(async () =>
+	{
+		try
+		{
+			await test();
+			return undefined;
+		}
+		catch (error)
+		{
+			return error;
+		}
+	});
+};
 
 export const coreTestRunner = async (): Promise<void> =>
 {
 	const timedResult = await timedAsyncCall(async () =>
 	{
-		await emptyAssertPassTest();
-		await exceptionalAssertFailTest();
-		await exceptionalActFailTest();
-		await exceptionalArrangeFailTest();
-		await exceptionalAnnulFailTest();
+		console.log(await runTest(emptyAssertPassTest));
+		console.log(await runTest(exceptionalAssertFailTest));
+		console.log(await runTest(exceptionalActFailTest));
+		console.log(await runTest(exceptionalArrangeFailTest));
+		console.log(await runTest(exceptionalAnnulFailTest));
 	});
-	console.log(`Test ran successfully in ${timedResult.elapsedNanoSeconds} nanoseconds`);
+	console.log(`Test run took in ${timedResult.elapsedNanoSeconds} nanoseconds`);
 };
