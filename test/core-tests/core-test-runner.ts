cfbb14effd10b28d02d7041fe32885f0d445bb2c
Blake Lindsay <blake@gotmonkey.com>
2020-04-03 19:10:51 -0700

    TCR improve output

--- test/core-tests/core-test-runner.ts
+++ test/core-tests/core-test-runner.ts
@@ -1,31 +1,51 @@
 import { timedAsyncCall, TimedResult } from "../../src/timing";
 import { emptyAssertPassTest, exceptionalActFailTest, exceptionalAnnulFailTest, exceptionalArrangeFailTest, exceptionalAssertFailTest } from "./";
 
-const runTest = async (test: () => Promise<void>): Promise<TimedResult<Error | string>> =>
+type TestResult = { passed: boolean; name: string; error: Error | undefined; };
+
+const runTest = async (test: () => Promise<void>): Promise<TimedResult<TestResult>> =>
 {
 	return await timedAsyncCall(async () =>
 	{
 		try
 		{
 			await test();
-			return test.name;
+			return { passed: true, name: test.name, error: undefined };
 		}
 		catch (error)
 		{
-			return error;
+			return { passed: false, name: test.name, error: error };
 		}
 	});
 };
 
+const evaluateTestRun = (timedResult: TimedResult<TestResult>): string =>
+{
+	return timedResult.result.error
+		? `\u001b[31mTest ${timedResult.result.name} failed in ${timedResult.elapsedNanoSeconds} nanoseconds with message: '${timedResult.result.error.message}' and stack trace: '${timedResult.result.error.stack}\u001b[0m'`
+		: `\u001b[32mTest ${timedResult.result.name} passed in ${timedResult.elapsedNanoSeconds} nanoseconds\u001b[0m`;
+};
+
+const logResults = (runResults: TimedResult<TimedResult<TestResult>[]>): void =>
+{
+	for (const testResult of runResults.result)
+	{
+		console.log(evaluateTestRun(testResult));
+	}
+	console.log(`\u001b[36mTest run took ${runResults.elapsedNanoSeconds} nanoseconds\u001b[0m`);
+};
+
 export const coreTestRunner = async (): Promise<void> =>
 {
-	const timedResult = await timedAsyncCall(async () =>
+	const runResults = await timedAsyncCall(async () =>
 	{
-		console.log(await runTest(emptyAssertPassTest));
-		console.log(await runTest(exceptionalAssertFailTest));
-		console.log(await runTest(exceptionalActFailTest));
-		console.log(await runTest(exceptionalArrangeFailTest));
-		console.log(await runTest(exceptionalAnnulFailTest));
+		return [
+			await runTest(emptyAssertPassTest),
+			await runTest(exceptionalArrangeFailTest),
+			await runTest(exceptionalActFailTest),
+			await runTest(exceptionalAssertFailTest),
+			await runTest(exceptionalAnnulFailTest)
+		];
 	});
-	console.log(`Test run took in ${timedResult.elapsedNanoSeconds} nanoseconds`);
+	logResults(runResults);
 };
