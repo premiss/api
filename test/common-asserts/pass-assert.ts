e7c778c92d16db16cc32b356e24fa2922e0bafd8
Blake Lindsay <blake@gotmonkey.com>
2020-04-03 17:17:11 -0700

    TCR refactor assertion

--- test/common-asserts/pass-assert.ts
+++ test/common-asserts/pass-assert.ts
@@ -1,8 +1,9 @@
 import { strict as assert } from "assert";
-import { ExamResult } from "../../src";
+import { StepExecutionError } from "../../src";
 
-export const passedAssert = (examResult: ExamResult): void =>
+export const passedAssert = (result: { passed: boolean; elapsedNanoseconds: bigint; stepExecutionError: StepExecutionError | undefined; }): void =>
 {
-	assert.equal(examResult.passed, true, "The exam should have passed");
-	assert.equal(examResult.stepExecutionError, undefined, "An passing result should have no error");
+	assert.equal(result.passed, true, "The result passed value should be true");
+	assert.ok(result.elapsedNanoseconds > 0, "The result elapsed nanoseconds should be 0");
+	assert.equal(result.stepExecutionError, undefined, "The result step execution error should be undefined");
 };
