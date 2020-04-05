74a1a32609b67cbed856356833445ae0ed0935d1
Blake Lindsay <blake@gotmonkey.com>
2020-04-05 13:16:06 -0700

    TCR improve assert message

--- test/common-asserts/timing-assert.ts
+++ test/common-asserts/timing-assert.ts
@@ -1,11 +1,18 @@
 import { strict as assert } from "assert";
 import { ExamResult, ProofStep, TimedResult } from "../../src";
+import { StepExecutionResultSet } from "../../src/step-execution";
+
+const sumAllStepsElapsedNanoseconds = (stepExecutionResultSet: StepExecutionResultSet): bigint =>
+{
+	return stepExecutionResultSet[ProofStep.act].elapsedNanoseconds
+		+ stepExecutionResultSet[ProofStep.arrange].elapsedNanoseconds
+		+ stepExecutionResultSet[ProofStep.assert].elapsedNanoseconds
+		+ stepExecutionResultSet[ProofStep.annul].elapsedNanoseconds;
+};
 
 export const timingAssert = (timedExamResult: TimedResult<ExamResult>): void =>
 {
-	assert.ok(timedExamResult.elapsedNanoseconds > 0, "An exam result should have timing");
-	assert.ok(timedExamResult.elapsedNanoseconds >= (timedExamResult.result.stepExecutionResultSet[ProofStep.act].elapsedNanoseconds
-		+ timedExamResult.result.stepExecutionResultSet[ProofStep.arrange].elapsedNanoseconds
-		+ timedExamResult.result.stepExecutionResultSet[ProofStep.assert].elapsedNanoseconds
-		+ timedExamResult.result.stepExecutionResultSet[ProofStep.annul].elapsedNanoseconds), "Exam elapsed nanoseconds should be greater than or equal to the sum of all steps time");
+	assert.ok(timedExamResult.elapsedNanoseconds > 0, "An exam result should have timing greater than zero");
+	const totalStepElapsedNanoseconds = sumAllStepsElapsedNanoseconds(timedExamResult.result.stepExecutionResultSet);
+	assert.ok(timedExamResult.elapsedNanoseconds >= totalStepElapsedNanoseconds, `Exam elapsed nanoseconds '${timedExamResult.elapsedNanoseconds}' should be greater than or equal to the sum of all steps time '${totalStepElapsedNanoseconds}'`);
 };
