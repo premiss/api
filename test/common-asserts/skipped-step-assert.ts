d7ae0c28b4691e95d161a70227366f67143d4feb
Blake Lindsay <blake@gotmonkey.com>
2020-04-05 13:13:38 -0700

    TCR improve assert message

--- test/common-asserts/skipped-step-assert.ts
+++ test/common-asserts/skipped-step-assert.ts
@@ -7,7 +7,7 @@ export const skippedStepAssert = (stepExecutionResultSet: StepExecutionResultSet
 	for (const proofStep of proofSteps)
 	{
 		assert.equal(stepExecutionResultSet[proofStep].result.passed, true, `The ${[proofStep]} passed value should be true`);
-		assert.equal(stepExecutionResultSet[proofStep].elapsedNanoseconds, BigInt(0), `The ${[proofStep]} elapsed nanoseconds value should be 0`);
-		assert.equal(stepExecutionResultSet[proofStep].result.executionError, undefined, `The ${[proofStep]} step execution error value should be undefined`);
+		assert.equal(stepExecutionResultSet[proofStep].elapsedNanoseconds, BigInt(0), `The ${[proofStep]} elapsed nanoseconds value should be 0, but was ${stepExecutionResultSet[proofStep].elapsedNanoseconds} nanoseconds`);
+		assert.equal(stepExecutionResultSet[proofStep].result.executionError, undefined, `The ${[proofStep]} execution error should be undefined, but was defined with then message ${(stepExecutionResultSet[proofStep].result.executionError?.error as Error)?.message}`);
 	}
 };
