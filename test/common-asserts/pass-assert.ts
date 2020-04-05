52663bc4131371d59796ffd0419e8887f320419b
Blake Lindsay <blake@gotmonkey.com>
2020-04-05 13:12:16 -0700

    TCR improve assert message

--- test/common-asserts/pass-assert.ts
+++ test/common-asserts/pass-assert.ts
@@ -4,6 +4,6 @@ import { ExecutionResult, TimedResult } from "../../src";
 export const passedAssert = (timedExecutionResult: TimedResult<ExecutionResult>): void =>
 {
 	assert.equal(timedExecutionResult.result.passed, true, "The result passed value should be true");
-	assert.ok(timedExecutionResult.elapsedNanoseconds > 0, "The result elapsed nanoseconds should be 0");
-	assert.equal(timedExecutionResult.result.executionError, undefined, "The result step execution error should be undefined");
+	assert.ok(timedExecutionResult.elapsedNanoseconds > 0, `The result elapsed nanoseconds should be 0, but was ${timedExecutionResult.elapsedNanoseconds} nanoseconds`);
+	assert.equal(timedExecutionResult.result.executionError, undefined, `The result execution error should be undefined, but was defined for step ${timedExecutionResult.result.executionError?.proofStep} with then message ${(timedExecutionResult.result.executionError?.error as Error)?.message}`);
 };
