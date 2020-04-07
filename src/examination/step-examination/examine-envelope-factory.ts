d71442910d97f5f25e048d6bd6cbb07b8eee86a0
Blake Lindsay <blake@gotmonkey.com>
2020-04-07 14:27:17 -0700

    TCR change dependecy to Examine interface

---
+++ src/examination/step-examination/examine-envelope-factory.ts
@@ -0,0 +1,11 @@
+import { Examine } from "../examine";
+import { StepExaminationResultSet } from "./step-examination-result-set";
+
+export const examineEnvelopeFactory = (innerExaminer: Examine, outerExaminer: Examine) =>
+{
+	return async (stepExaminationResultSet: StepExaminationResultSet): Promise<StepExaminationResultSet> =>
+	{
+		stepExaminationResultSet = await innerExaminer(stepExaminationResultSet);
+		return outerExaminer(stepExaminationResultSet);
+	};
+};
--- src/examination/step-examination/step-examiner-chain-factory.ts
+++ src/examination/step-examination/step-examiner-chain-factory.ts
@@ -1,8 +1,8 @@
 import { Proof, ProofStep } from "../../index";
 import { Examine } from "../examine";
 import { examineStepFactory } from "./.";
+import { examineEnvelopeFactory } from "./examine-envelope-factory";
 import { examinePassThru } from "./examine-pass-thru";
-import { StepExaminerEnvelope } from "./step-examiner-envelope";
 
 export const stepExaminerChainFactory = (proof: Proof): Examine =>
 {
@@ -10,5 +10,5 @@ export const stepExaminerChainFactory = (proof: Proof): Examine =>
 	const examineAct = examineStepFactory(ProofStep.act, proof[ProofStep.act], examineAssert);
 	const examineArrange = examineStepFactory(ProofStep.arrange, proof[ProofStep.arrange], examineAct);
 	const examineAnnul = examineStepFactory(ProofStep.annul, proof[ProofStep.annul], examinePassThru);
-	return (stepExaminationResultSet) => (new StepExaminerEnvelope(examineArrange, examineAnnul)).probe(stepExaminationResultSet);
+	return examineEnvelopeFactory(examineArrange, examineAnnul);
 };
