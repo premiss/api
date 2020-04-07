9dbf4857927a78cdabd62d3eaf8f2dfe4c2fe835
Blake Lindsay <blake@gotmonkey.com>
2020-04-07 16:00:35 -0700

    TCR rename step executor factory

--- src/examination/step-examination/examine-step-execution-factory.ts
+++ src/examination/step-examination/examine-step-execution-factory.ts
@@ -1,15 +1,15 @@
 import { Examine } from "../examine";
 import { StepExaminationResultSet } from "./.";
 import { stepExaminationResultSetFactory } from "./step-examination-result-set-factory";
-import { stepExecutorResultFactory } from "./step-executor-result-factory";
+import { stepExaminationResultFactory } from "./step-examination-result-factory";
 import { StepSubject } from "./step-subject";
 
 export const examineStepExecutionFactory = (subject: StepSubject, nextStepExamine: Examine) =>
 {
 	return async (stepExaminationResultSet: StepExaminationResultSet): Promise<StepExaminationResultSet> =>
 	{
-		const stepExecutorResult = await stepExecutorResultFactory(subject, nextStepExamine);
-		stepExaminationResultSet = stepExaminationResultSetFactory(stepExaminationResultSet, subject.proofStep, stepExecutorResult.examinationResult);
-		return stepExecutorResult.nextStepExamine(stepExaminationResultSet);
+		const stepExaminationResult = await stepExaminationResultFactory(subject, nextStepExamine);
+		stepExaminationResultSet = stepExaminationResultSetFactory(stepExaminationResultSet, subject.proofStep, stepExaminationResult.examinationResult);
+		return stepExaminationResult.nextStepExamine(stepExaminationResultSet);
 	};
 };
--- src/examination/step-examination/step-examination-result-factory.ts
+++ src/examination/step-examination/step-examination-result-factory.ts
@@ -17,7 +17,7 @@ const createErredExaminationResult = (error: unknown, proofStep: ProofStep): Exa
 	return { passed, examinationError: executionError };
 };
 
-export const stepExecutorResultFactory = async (subject: StepSubject, nextStepExamine: Examine): Promise<StepExaminationResult> =>
+export const stepExaminationResultFactory = async (subject: StepSubject, nextStepExamine: Examine): Promise<StepExaminationResult> =>
 {
 	const examinationResult = await timedAsyncCall(async () =>
 	{
