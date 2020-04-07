653633d756610f4307d9e512eeba614b956b55ca
Blake Lindsay <blake@gotmonkey.com>
2020-04-07 15:09:28 -0700

    TCR rename step result set factory

--- src/examination/step-examination/examine-step-execution-factory.ts
+++ src/examination/step-examination/examine-step-execution-factory.ts
@@ -1,6 +1,6 @@
 import { Examine } from "../examine";
 import { StepExaminationResultSet } from "./.";
-import { stepExecutionResultSetFactory } from "./step-execution-result-set-factory";
+import { stepExaminationResultSetFactory } from "./step-examination-result-set-factory";
 import { stepExecutorResultFactory } from "./step-executor-result-factory";
 import { StepSubject } from "./step-subject";
 
@@ -9,7 +9,7 @@ export const examineStepExecutionFactory = (subject: StepSubject, nextStepExamin
 	return async (stepExaminationResultSet: StepExaminationResultSet): Promise<StepExaminationResultSet> =>
 	{
 		const stepExecutorResult = await stepExecutorResultFactory(subject, nextStepExamine);
-		stepExaminationResultSet = stepExecutionResultSetFactory(stepExaminationResultSet, subject.proofStep, stepExecutorResult.examinationResult);
+		stepExaminationResultSet = stepExaminationResultSetFactory(stepExaminationResultSet, subject.proofStep, stepExecutorResult.examinationResult);
 		return stepExecutorResult.nextStepExamine(stepExaminationResultSet);
 	};
 };
--- src/examination/step-examination/step-examination-result-set-factory.ts
+++ src/examination/step-examination/step-examination-result-set-factory.ts
@@ -1,7 +1,7 @@
 import { ExaminationResult, ProofStep, TimedResult } from "../../index";
 import { StepExaminationResultSet } from "./.";
 
-export const stepExecutionResultSetFactory = (stepExaminationResultSet: StepExaminationResultSet, proofStep: ProofStep, timedExaminationResult: TimedResult<ExaminationResult>): StepExaminationResultSet =>
+export const stepExaminationResultSetFactory = (stepExaminationResultSet: StepExaminationResultSet, proofStep: ProofStep, timedExaminationResult: TimedResult<ExaminationResult>): StepExaminationResultSet =>
 {
 	return { ...stepExaminationResultSet, [proofStep]: timedExaminationResult };
 };
