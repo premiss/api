b6ea85403c35ae5eaef4494a3d3437691eae3a34
Blake Lindsay <blake@gotmonkey.com>
2020-04-06 22:38:46 -0700

    TCR extract examine step factory

---
+++ src/examination/step-examination/examine-step-factory.ts
@@ -0,0 +1,12 @@
+import { ProofStep } from "../../proof-step";
+import { ProofStepSignature } from "../../proof-step-signature";
+import { SkipStepExaminer } from "./skip-step-examiner";
+import { StepExaminer } from "./step-examiner";
+import { StepExecutor } from "./step-executor";
+
+export const examineStepFactory = (proofStep: ProofStep, proofStepSignature: ProofStepSignature | undefined, nextStepExaminer: StepExaminer): StepExaminer =>
+{
+	return proofStepSignature
+		? new StepExecutor({ proofStep, proofStepSignature }, nextStepExaminer)
+		: new SkipStepExaminer(proofStep, nextStepExaminer);
+};
--- src/examination/step-examination/step-examiner-chain-factory.ts
+++ src/examination/step-examination/step-examiner-chain-factory.ts
@@ -1,22 +1,13 @@
-import { Proof, ProofStep, ProofStepSignature } from "../../index";
-import { StepExaminer } from "./.";
+import { Proof, ProofStep } from "../../index";
+import { examineStepFactory, StepExaminer } from "./.";
 import { endStepExaminer } from "./end-step-examiner";
-import { SkipStepExaminer } from "./skip-step-examiner";
 import { StepExaminerEnvelope } from "./step-examiner-envelope";
-import { StepExecutor } from "./step-executor";
-
-const stepExaminerFactory = (proofStep: ProofStep, proofStepSignature: ProofStepSignature | undefined, nextStepExaminer: StepExaminer): StepExaminer =>
-{
-	return proofStepSignature
-		? new StepExecutor({ proofStep, proofStepSignature }, nextStepExaminer)
-		: new SkipStepExaminer(proofStep, nextStepExaminer);
-};
 
 export const stepExaminerChainFactory = (proof: Proof): StepExaminer =>
 {
-	const assertExaminer = stepExaminerFactory(ProofStep.assert, proof[ProofStep.assert], endStepExaminer);
-	const actExaminer = stepExaminerFactory(ProofStep.act, proof[ProofStep.act], assertExaminer);
-	const arrangeExaminer = stepExaminerFactory(ProofStep.arrange, proof[ProofStep.arrange], actExaminer);
-	const annulExaminer = stepExaminerFactory(ProofStep.annul, proof[ProofStep.annul], endStepExaminer);
+	const assertExaminer = examineStepFactory(ProofStep.assert, proof[ProofStep.assert], endStepExaminer);
+	const actExaminer = examineStepFactory(ProofStep.act, proof[ProofStep.act], assertExaminer);
+	const arrangeExaminer = examineStepFactory(ProofStep.arrange, proof[ProofStep.arrange], actExaminer);
+	const annulExaminer = examineStepFactory(ProofStep.annul, proof[ProofStep.annul], endStepExaminer);
 	return new StepExaminerEnvelope(arrangeExaminer, annulExaminer);
 };
