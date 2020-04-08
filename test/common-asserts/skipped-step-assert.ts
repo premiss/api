474b557b5f952ec3bd7b97a2641467e41bda05ec
Blake Lindsay <blake@gotmonkey.com>
2020-04-07 19:36:37 -0700

    TCR convert to forEach

--- test/common-asserts/skipped-step-assert.ts
+++ test/common-asserts/skipped-step-assert.ts
@@ -1,13 +1,15 @@
 import { strict as assert } from "assert";
-import { ProofStep } from "../../src";
+import { ExaminationResult, ProofStep, TimedResult } from "../../src";
 import { StepExaminationResultSet } from "../../src/examination/step-examination";
+
+const assertSkippedStep = (proofStep: ProofStep, timedExaminationResult: TimedResult<ExaminationResult>): void =>
+{
+	assert.equal(timedExaminationResult.result.passed, true, `The ${[proofStep]} passed value should be true`);
+	assert.equal(timedExaminationResult.elapsedNanoseconds, BigInt(0), `The ${[proofStep]} elapsed nanoseconds value should be 0, but was ${timedExaminationResult.elapsedNanoseconds} nanoseconds`);
+	assert.equal(timedExaminationResult.result.examinationError, undefined, `The ${[proofStep]} examination error should be undefined, but was defined with then message ${(timedExaminationResult.result.examinationError?.error as Error)?.message}`);
+};
 
 export const skippedStepAssert = (stepExaminationResultSet: StepExaminationResultSet, ...proofSteps: readonly ProofStep[]): void =>
 {
-	for (const proofStep of proofSteps)
-	{
-		assert.equal(stepExaminationResultSet[proofStep].result.passed, true, `The ${[proofStep]} passed value should be true`);
-		assert.equal(stepExaminationResultSet[proofStep].elapsedNanoseconds, BigInt(0), `The ${[proofStep]} elapsed nanoseconds value should be 0, but was ${stepExaminationResultSet[proofStep].elapsedNanoseconds} nanoseconds`);
-		assert.equal(stepExaminationResultSet[proofStep].result.examinationError, undefined, `The ${[proofStep]} execution error should be undefined, but was defined with then message ${(stepExaminationResultSet[proofStep].result.examinationError?.error as Error)?.message}`);
-	}
+	proofSteps.forEach(proofStep => assertSkippedStep(proofStep, stepExaminationResultSet[proofStep]));
 };
