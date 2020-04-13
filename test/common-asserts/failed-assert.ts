9f12459c1511e9275b4feb96b5cee76e48d28670
Blake Lindsay <blake@gotmonkey.com>
2020-04-12 17:36:13 -0700

    TCR clean up

--- test/common-asserts/failed-assert.ts
+++ test/common-asserts/failed-assert.ts
@@ -12,9 +12,9 @@ const error = new Error("Kaboom!?");
 export const failureAssertion = ((examResult: ProofExaminationResult, proofStep: ProofStep): void =>
 {
 	assert.equal(examResult.passed, false, `An exception thrown during ${proofStep} should fail`);
-	assert.ok(examResult.examinationError, "The exam execution error is not defined");
-	assert.equal((examResult.examinationError?.error as Error), error, `The execution error should have been the ${error.message} error`);
-	assert.equal(examResult.examinationError?.proofStep, proofStep, `The execution error should be for step ${proofStep} but value was ${examResult.examinationError?.proofStep || "undefined"}`);
+	assert.ok(examResult.examinationError, "The examination error is not defined");
+	assert.equal((examResult.examinationError?.error as Error), error, `The examination error should have been the ${error.message} error`);
+	assert.equal(examResult.examinationError?.proofStep, proofStep, `The examination error should be for step ${proofStep} but value was ${examResult.examinationError?.proofStep || "undefined"}`);
 }) as FailureAssertion;
 failureAssertion.error = error;
 
--- test/common-asserts/pass-assert.ts
+++ test/common-asserts/pass-assert.ts
@@ -6,5 +6,5 @@ export const passedAssert = (timedExecutionResult: TimedResult<ExaminationResult
 {
 	assert.equal(timedExecutionResult.result.passed, true, "The result passed value should be true");
 	assert.ok(timedExecutionResult.elapsedNanoseconds > 0, `The result elapsed nanoseconds should be 0, but was ${timedExecutionResult.elapsedNanoseconds} nanoseconds`);
-	assert.equal(timedExecutionResult.result.examinationError, undefined, `The result execution error should be undefined, but was defined for step ${timedExecutionResult.result.examinationError?.proofStep} with then message ${(timedExecutionResult.result.examinationError?.error as Error)?.message}`);
+	assert.equal(timedExecutionResult.result.examinationError, undefined, `The result examination error should be undefined, but was defined for step ${timedExecutionResult.result.examinationError?.proofStep} with then message ${(timedExecutionResult.result.examinationError?.error as Error)?.message}`);
 };
