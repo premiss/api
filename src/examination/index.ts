a0ea47e2e3d1b5d441da9ea523cecd8d8341cbcc
Blake Lindsay <blake@gotmonkey.com>
2020-04-12 17:47:24 -0700

    TCR clean up

--- src/examination/index.ts
+++ src/examination/index.ts
@@ -4,5 +4,5 @@ export { Examine } from "./examine";
 export { examinePassThru } from "./examine-pass-thru";
 export { examineEnvelopeFactory } from "./examine-envelope-factory";
 export { proofExaminationErrorFactory } from "./proof-examination-error-factory";
-export { proofExamineFactory } from "./proof-examination-factory";
+export { proofExamineFactory } from "./proof-examine-factory";
 export { examineProof } from "./examine-proof";
