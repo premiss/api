c5992fdf7d4baded82bc4773c99b16f9ab2f1ef7
Blake Lindsay <blake@gotmonkey.com>
2020-04-12 17:39:22 -0700

    TCR clean up

--- src/step-examination/examine-step-factory.ts
+++ src/step-examination/examine-step-factory.ts
@@ -2,14 +2,9 @@ import { Proof, ProofStep, ProofStepSignature } from "../";
 import { Examine } from "../examination";
 import { examineStepExecutionFactory, examineStepSkipFactory } from "./";
 
-const createProofStepSignature = (proof: Proof, proofStepSignature: ProofStepSignature): ProofStepSignature =>
-{
-	return (): Promise<void> => proofStepSignature.call(proof);
-};
-
 export const examineStepFactory = (proof: Proof, proofStep: ProofStep, proofStepSignature: ProofStepSignature | undefined, nextStepExamine: Examine): Examine =>
 {
 	return proofStepSignature
-		? examineStepExecutionFactory({ proofStep, proofStepSignature: createProofStepSignature(proof, proofStepSignature) }, nextStepExamine)
+		? examineStepExecutionFactory({ proofStep, proofStepSignature: (): Promise<void> => proofStepSignature.call(proof) }, nextStepExamine)
 		: examineStepSkipFactory(proofStep, nextStepExamine);
 };
