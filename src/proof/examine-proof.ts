0ff485be786fa1c684f1ba010d60ca3052c3ef58
Blake Lindsay <blake@gotmonkey.com>
2020-04-15 20:28:44 -0700

    TCR refactor

--- src/proof/examine-proof.ts
+++ src/proof/examine-proof.ts
@@ -1,11 +1,12 @@
-import { emptyExaminationResultSet } from "../examination";
-import { Proof, proofExaminationErrorFactory, ProofExaminationResult, proofExamineFactory, proofOutcomeFactory } from "./";
+import { emptyExaminationResultSet, ExaminationOutcomeObserved } from "../examination";
+import { Proof, proofExaminationErrorFactory, ProofExaminationResult, proofExamineFactory } from "./";
 
 export const examineProof = async (proof: Proof): Promise<ProofExaminationResult> =>
 {
 	const examine = proofExamineFactory(proof);
 	const examinationResultSet = await examine(emptyExaminationResultSet);
-	const examinationOutcome = proofOutcomeFactory(examinationResultSet);
 	const examinationError = proofExaminationErrorFactory(examinationResultSet);
-	return { examinationOutcome, examinationError, examinationResultSet } as ProofExaminationResult;
+	return examinationError
+		? { examinationOutcome: ExaminationOutcomeObserved.Failed, examinationError, examinationResultSet }
+		: { examinationOutcome: ExaminationOutcomeObserved.Passed, examinationError, examinationResultSet };
 };
--- src/proof/index.ts
+++ src/proof/index.ts
@@ -2,7 +2,6 @@ export { ProofStep } from "./proof-step";
 export { ProofStepSignature } from "./proof-step-signature";
 export { Proof } from "./proof";
 export { ProofExaminationResult } from "./proof-examination-result";
-export { proofOutcomeFactory } from "./proof-outcome-factory";
 export { proofExaminationErrorFactory } from "./proof-examination-error-factory";
 export { proofExamineFactory } from "./proof-examine-factory";
 export { examineProof } from "./examine-proof";
