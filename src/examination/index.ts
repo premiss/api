63cd76650ebd32742f108411268b25abd4d500a8
Blake Lindsay <blake@gotmonkey.com>
2020-04-24 09:30:55 -0700

    TCR fix file name

--- src/examination/index.ts
+++ src/examination/index.ts
@@ -2,7 +2,7 @@ export { ExaminationOutcomeObserved, ExaminationOutcomeUnobserved, ExaminationOu
 export { ExaminationError } from "./examination-error";
 export { UnknownExaminationResult, ErredExaminationResult, PassedExaminationResult, ExaminationResult } from "./examination-result";
 export { ExaminationResultSet } from "./examination-result-set";
-export { emptyExaminationResultSet } from "./empty-step-examination-result-set";
+export { emptyExaminationResultSet } from "./empty-examination-result-set";
 export { examinationResultSetFactory } from "./examination-result-set-factory";
 export { Examine } from "./examine";
 export { examinePassThru } from "./examine-pass-thru";
