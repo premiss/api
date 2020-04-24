1c56c288fb7b4a0408fb3cc00367befb1a860554
Blake Lindsay <blake@gotmonkey.com>
2020-04-24 09:48:18 -0700

    TCR include in index export

--- src/proof/index.ts
+++ src/proof/index.ts
@@ -1,7 +1,7 @@
 export { ProofStep } from "./proof-step";
 export { ProofStepSignature } from "./proof-step-signature";
 export { Proof } from "./proof";
-export { ProofExaminationResult } from "./proof-examination-result";
+export { ErredProofExaminationResult, PassedProofExaminationResult, ProofExaminationResult } from "./proof-examination-result";
 export { proofExaminationErrorFactory } from "./proof-examination-error-factory";
 export { proofExamineFactory } from "./proof-examine-factory";
 export { examineProof } from "./examine-proof";
