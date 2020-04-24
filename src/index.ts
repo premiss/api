b7bfa15e972186b0e5e42973dfac22b5ba4ff35d
Blake Lindsay <blake@gotmonkey.com>
2020-04-24 09:48:56 -0700

    TCR include in index export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { verify } from "./verify";
+export { TimedResult } from "./timing";
+export { ExaminationOutcomeObserved, ExaminationOutcomeUnobserved, ExaminationOutcome, ExaminationError, ExaminationResultSet, UnknownExaminationResult, SkippedExaminationResult, ErredExaminationResult, PassedExaminationResult, ExaminationResult } from "./examination";
+export { ProofStep, ProofStepSignature, Proof, ErredProofExaminationResult, PassedProofExaminationResult, ProofExaminationResult } from "./proof";
