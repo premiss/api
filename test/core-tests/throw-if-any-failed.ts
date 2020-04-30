e577ed5b36617fbfeb671fdcd22128b3a37ab73e
Blake Lindsay <blake@gotmonkey.com>
2020-04-29 22:58:32 -0700

    TCR use every

--- test/core-tests/throw-if-any-failed.ts
+++ test/core-tests/throw-if-any-failed.ts
@@ -3,8 +3,8 @@ import { TestResult } from "./test-result";
 
 export const throwIfAnyFailed = (runResults: TimedResult<TimedResult<TestResult>[]>): void =>
 {
-	if (runResults.result.find(testResult => !testResult.result.passed))
+	if (!runResults.result.every(testResult => testResult.result.passed))
 	{
-		throw  new Error("Not all tests passed");
+		throw new Error("Not all tests passed");
 	}
 };
